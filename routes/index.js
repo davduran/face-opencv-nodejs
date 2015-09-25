5df8e1e97acf6571be98e8b0fae94c5f48f4ace2
David Duran <dduran@inform08.seic.local>
2015-09-25 17:39:16 +0200

    no message

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,17 @@ var cv = require('opencv');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    // (B)lue, (G)reen, (R)ed
+var lower_threshold = [46, 57, 83];
+var upper_threshold = [80, 96, 115];
+
 cv.readImage("./prova.png", function(err, im) {
   if (err) throw err;
+  if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
 
-  var width = im.width();
-  var height = im.height();
-  if (width < 1 || height < 1) throw new Error('Image has no size');
-
-  var srcArray = [0, 0, width, 0, width, height, 0, height];
-  var dstArray = [0, 0, width * 0.9, height * 0.1, width, height, width * 0.2, height * 0.8];
-  var xfrmMat = im.getPerspectiveTransform(srcArray, dstArray);
-  im.warpPerspective(xfrmMat, width, height, [255, 255, 255]);
-  im.save("./warp-image.png");
-  console.log('Image saved to ./warp-image.png');
+  im.inRange(lower_threshold, upper_threshold);
+  im.save('./tmp/coin_detected.jpg');
+  console.log('Image saved to ./tmp/coin_detected.jpg');
 });
   res.render('index', { title: 'Express' });
 });
