a9cbc9a5bfe9a5c1415272c3ceaf959ed28e7ecb
David Duran <dduran@inform08.seic.local>
2015-09-25 17:30:00 +0200

    no message

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,26 @@ var cv = require('opencv');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     cv.readImage("./prova.png", function(err, im) {
+      if (err) throw err;
+      if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
 
-        console.log(im);
-        res.render('index', { im: im });
+      img_hsv = im.copy();
+      img_gray = im.copy();
+
+      img_hsv.convertHSVscale();
+      img_gray.convertGrayscale();
+
+      im.save('./nor.png');
+      img_hsv.save('./hsv.png');
+      img_gray.save('./gray.png');
+
+      img_crop = im.crop(50,50,250,250);
+      img_crop.save('./crop.png');
+
+      console.log('Image saved to ./{crop|nor|hsv|gray}.png');
     });
 
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
