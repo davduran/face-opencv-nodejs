3daf73605c11dfdcab6dd4afeeff9e03f2c953c8
David Duran <dduran@inform08.seic.local>
2015-09-28 11:48:55 +0200

    no message

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,6 @@ var cv = require('opencv');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-/*cv.readImage("./public/prova.png", function(err, im) {
-  if (err) throw err;
-  if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
-
-  im.detectObject("./public/cascade.xml", {}, function(err, faces){
-    if (err) throw err;
-
-    for (var i = 0; i < faces.length; i++){
-      var face = faces[i];
-      im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
-    }
-
-    im.save('./public/face-detection.png');
-    console.log('Image saved to ./public/face-detection.png');
-
-      res.render('index', { faces: faces.length});
-  });
-  });
-*/
-
 var COLOR = [0, 255, 0]; // default red
 var thickness = 2; // default 1
 
@@ -38,10 +18,12 @@ cv.readImage('./public/prova.png', function(err, im) {
     for (var i = 0; i < faces.length; i++) {
       face = faces[i];
       im.rectangle([face.x, face.y], [face.width, face.height], COLOR, 2);
+
+      var im2 = im.roi(face.x, face.y, face.width, face.height)
+      im2.save('./public/face'+i+'.jpg')
     }
 
     im.save('./public/face-detection.png');
-    console.log('Image saved to ./public/face-detection-rectangle.png');
     res.render('index', { faces: faces.length});
   });
 
