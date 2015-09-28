eb3d6ed6312a47185da0a08a6f534a83428a6b90
David Duran <dduran@inform08.seic.local>
2015-09-28 11:20:16 +0200

    no message

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var cv = require('opencv');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-cv.readImage("./public/prova.png", function(err, im) {
+/*cv.readImage("./public/prova.png", function(err, im) {
   if (err) throw err;
   if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
 
@@ -23,6 +23,27 @@ cv.readImage("./public/prova.png", function(err, im) {
       res.render('index', { faces: faces.length});
   });
   });
+*/
+
+try {
+  var camera = new cv.VideoCapture(0);
+  var window = new cv.NamedWindow('Video', 0)
+
+  setInterval(function() {
+    camera.read(function(err, im) {
+      if (err) throw err;
+      console.log(im.size())
+      if (im.size()[0] > 0 && im.size()[1] > 0){
+        window.show(im);
+      }
+      window.blockingWaitKey(0, 50);
+    });
+  }, 20);
+
+} catch (e){
+  console.log("Couldn't start camera:", e)
+}
+
 });
 
 module.exports = router;
